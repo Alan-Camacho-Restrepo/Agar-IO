896afe03a18aac06a1c15f1947c42ac0ac35d354
Alan-Camacho-Restrepo <alans.camacho@udea.edu.co>
2022-09-09 10:10:46 -0500

    Scenas updates

--- agario/scenas/settingscene.py
+++ agario/scenas/settingscene.py
@@ -4,21 +4,16 @@ from agario.config import settings
 
 import pygame as pg
 
+
 class SettingScene(Scene):
-    
     def __init__(self):
         self
-    
-    def update(self, events):
 
-        if len(events) > 0:
-            for event in events:
-                if event.type == pg.KEYDOWN and event.key == pg.K_s:
-                    return SCENES.GAME
-                else:
-                    return SCENES.SETTINGS
-        else:
-            return SCENES.SETTINGS
+    def update(self, events):
+        for event in events:
+            if event.type == pg.KEYDOWN and event.key == pg.K_s:
+                return SCENES.GAME
+        return SCENES.SETTINGS
 
     def draw(self, screen):
         # set_w = settings.screen_width - 400
@@ -27,9 +22,7 @@ class SettingScene(Scene):
         set_h = 600
         surface = pg.Surface((set_w, set_h))
         surf_rect = surface.get_rect()
-        surf_rect.center = (settings.screen_width / 2, settings.screen_height / 2)
+        surf_rect.center = (settings.screen_width / 2, 
+                            settings.screen_height / 2)
         surface.fill((0, 155, 0))
-
         screen.blit(surface, surf_rect)
-
-    
--- agario/scenas/titlescene.py
+++ agario/scenas/titlescene.py
@@ -6,25 +6,19 @@ import pygame as pg
 
 pg.font.init()
 class TitleScene(Scene):
-    
-    def update(self, events):
-    
-        if len(events) != 0:
-            for event in events:
-                if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                    return SCENES.GAME
-                else:
-                    return SCENES.TITLE
-        else:
-            return SCENES.TITLE
-                        
-    
-    def draw(self, screen):
-        
-        font = pg.font.SysFont('chalkduster.ttf', 72)
-        text = font.render('Press SPACE to play', True, (0, 0, 0))
-        textRect = text.get_rect()
-        textRect.center = (settings.screen_width / 2, settings.screen_height / 2)
-        screen.fill((100, 140, 145))
-        screen.blit(text, textRect)
+    def __init__(self):
+        font = pg.font.SysFont('chalkduster.ttf', 72)    
+        self.text = font.render('Press SPACE to play', True, (0, 0, 0))
+        self.textRect = self.text.get_rect()
+        self.textRect.center = (settings.screen_width / 2, 
+                           settings.screen_height / 2)
 
+    def update(self, events):
+        for event in events:
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                return SCENES.GAME
+        return SCENES.TITLE
+
+    def draw(self, screen):
+        screen.fill((100, 140, 145))
+        screen.blit(self.text, self.textRect)
